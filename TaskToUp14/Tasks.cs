a9becccc0ab628b39fe6a79831d960673cfd8121
Alex <Utin97@mail.ru>
2023-01-07 18:37:03 +0300

    Решил задачу №0

--- TaskToUp14/Tasks.cs
+++ TaskToUp14/Tasks.cs
@@ -1,8 +1,12 @@
 class Tasks
 {
+    /// <summary>
+    /// Задача 0. Вывести квадрат числа.
+    /// </summary>
     public static void Task0()
     {
+        Console.WriteLine("Введите число, которое необходимо возвести в квадрат:");
         int number = MyMethods.InputNumberInt();
-        MyMethods.SquareNumber(number);
+        Console.WriteLine($"Квадрат числа {number} равен: {MyMethods.SquareNumber(number)}.");
     }
 }
